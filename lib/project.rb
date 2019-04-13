75f5376e1928fc659a4d5d413624566c376d5219
Ryan Pethel <arpethel@gmail.com>
2019-04-13 17:00:54 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Project
+  attr_accessor :backer 
+
+  @@backers = []
+
+  def initialize
+    @backer = backer
+  end
+
+  def add_backer(backer)
+    @@backers << backer
+  end
+end
