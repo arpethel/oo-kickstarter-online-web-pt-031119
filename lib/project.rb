a3d55e776b355a07d3a83defba9e759475e208a7
Ryan Pethel <arpethel@gmail.com>
2019-04-13 17:08:58 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,19 @@
 require 'pry'
 
 class Project
-  attr_accessor :backer
+  attr_accessor :backer, :backers
+  attr_reader :title
 
   @@backers = []
 
-  def initialize
+  def initialize(title)
+    @title = title
     @backer = backer
+    @backers = []
+  end
+
+  def project_title=(title)
+    @title = title
   end
 
   def add_backer(backer)
